e00aa4473962c82b95073f1dbcfde84e63d42f8d
Himanshu Bhatt <66287451+desininja@users.noreply.github.com>
2020-12-02 22:56:23 +0530

    Update study_hour_vs_marks.py

--- study_hour_vs_marks.py
+++ study_hour_vs_marks.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import plotly.express as px
-#from plotly.subplots import make_subplots
-#import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 
 import pickle
@@ -29,12 +29,12 @@ def load_data():
 data = load_data()
 
 
-#def plots(data):
-#    st.subheader("Study hours Vs Marks Scatter plot")
-#    fig = px.scatter(data, x = 'Hours', y = 'Scores')
-#    st.plotly_chart(fig)
+def plots(data):
+    st.subheader("Study hours Vs Marks Scatter plot")
+    fig = px.scatter(data, x = 'Hours', y = 'Scores')
+    st.plotly_chart(fig)
     
-#plots(data)
+plots(data)
 
 
 def prediction_time(h):
