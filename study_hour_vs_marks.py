4088471a732578bf01e8361c26e081aa7d1796c3
Himanshu Bhatt <66287451+desininja@users.noreply.github.com>
2020-12-02 23:01:09 +0530

    Update study_hour_vs_marks.py

--- study_hour_vs_marks.py
+++ study_hour_vs_marks.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
+
+
 
 
 import pickle
